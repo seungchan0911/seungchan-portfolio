441b7893b2ff026c5f7a55b7d85d5805552352a1
seungchan0911 <167081641+seungchan0911@users.noreply.github.com>
2025-08-20 22:14:52 +0900

    Update AdminAuth.php

--- app/Http/Middleware/AdminAuth.php
+++ app/Http/Middleware/AdminAuth.php
@@ -16,7 +16,7 @@ class AdminAuth
     public function handle(Request $request, Closure $next): Response
     {
         if (!session('admin_logged_in')) {
-            return redirect('admin.login');
+            return redirect()->route('login');
         }
 
         return $next($request);
