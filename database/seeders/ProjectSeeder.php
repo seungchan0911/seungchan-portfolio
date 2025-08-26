3a3e1dec3e20183f827a7dca2d129c5b6803a9da
seungchan0911 <imrealsexy00@gmail.com>
2025-08-26 20:05:30 +0900

    Update project image paths and seed data
    Renamed image files for projects to use simplified names and updated the ProjectSeeder to reference the new image paths. This improves consistency and clarity in image asset management.

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -14,11 +14,11 @@ class ProjectSeeder extends Seeder
     public function run(): void
     {
         Project::insert([
-            // ['title'=>'povoko studio','image_path'=>'img/source/www.povokostudio.com_.png','link_url'=>'http://www.povokostudio.com','published_at'=>'2025-09-11'],
-            // ['title'=>'cactus curry club','image_path'=>'img/source/cactus-curry-club.vercel.app_.png','link_url'=>'http://cactus-curry-club.vercel.app','published_at'=>'2025-05-10.'],
-            // ['title'=>'columnfort','image_path'=>'img/source/columnfort.png','link_url'=>'','published_at'=>'2025-06-20'],
-            // ['title'=>'enforce sword game','image_path'=>'img/source/enforce-sword-game.vercel.app_(desktop).png','link_url'=>'https://enforce-sword-game.vercel.app/','published_at'=>'2025-04-25'],
-            // ['title'=>'I\'ll fill it up soon','image_path'=>'img/source/sample-image.png','link_url'=>'','published_at'=>null],
+            ['title'=>'povoko studio','image_path'=>'img/source/povoko-studio.png','link_url'=>'http://www.povokostudio.com','published_at'=>'2025-09-11'],
+            ['title'=>'cactus curry club','image_path'=>'img/source/cactus-curry-club.png','link_url'=>'http://cactus-curry-club.vercel.app','published_at'=>'2025-05-10.'],
+            ['title'=>'columnfort','image_path'=>'img/source/columnfort.png','link_url'=>'','published_at'=>'2025-06-20'],
+            ['title'=>'enforce sword game','image_path'=>'img/source/enforce-sword-game.png','link_url'=>'https://enforce-sword-game.vercel.app/','published_at'=>'2025-04-25'],
+            ['title'=>'I\'ll fill it up soon','image_path'=>'img/source/sample-image.png','link_url'=>'','published_at'=>null],
             ['title'=>'frame24','image_path'=>'img/source/frame24.png','link_url'=>'http://frame24.laravel.cloud','published_at'=>'2025-07-05'],
         ]);
     }
